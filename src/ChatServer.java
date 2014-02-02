7206c805bc76c1b0a09d31fc7d8aaeeb999af646
samshuster <samshuste@gmail.com>
2014-02-01 16:38:26 -0800

    Now is threaded

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
@@ -7,64 +6,85 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
 
-public class ChatServer {
+public class ChatServer implements Runnable {
 	private ServerSocket service = null;
-	private List<Socket> socketList = null;
-	private DataInputStream streamIn = null; 
-	
-	public ChatServer(int portNum){
-		try{
+	private Socket socket = null;
+	private DataInputStream streamIn = null;
+	private Thread thread = null;
+
+	public ChatServer(int portNum) {
+		try {
 			System.out.println("Creating a Chat Server on portNum: " + portNum);
 			service = new ServerSocket(portNum);
 			System.out.println("Chat Server Started");
 			System.out.println("Waiting for a client");
 			System.out.println(service.getInetAddress());
-			
 			System.out.println("Success!");
+			start();
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	private void start() {
+		if (thread == null) {
+			thread = new Thread(this);
+			thread.start();
+		}
+	}
+	
+	private void open() throws IOException {
+		this.streamIn = new DataInputStream(new BufferedInputStream(
+				this.socket.getInputStream()));
+	}
+
+	private void close() throws IOException {
+		if (streamIn != null) {
+			streamIn.close();
+		}
+		if (socket != null) {
+			socket.close();
+		}
+		if (service != null) {
+			service.close();
+		}
+	}
+	
+	
+
+	@Override
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			socket = service.accept();
 			open();
 			boolean done = false;
-			while (!done){
-				try{
+			while (!done) {
+				try {
 					String curLine = streamIn.readUTF();
 					System.out.println(curLine);
 					done = curLine.equals(".bye");
-				} catch(IOException ioe){
+				} catch (IOException ioe) {
 					done = true;
 				}
 			}
 			close();
-		}
-		catch (IOException e){
-			System.out.println(e);
-		}
-	}
-	
-	private void searchForThreads(){
-		//Will keep looking for a Client until one is found and then will proceed
-		socket = service.accept();
-		Thread f;
-	}
-	
-	private void open() throws IOException{
-		this.streamIn = new DataInputStream(new BufferedInputStream(this.socket.getInputStream()));
-	}
-	
-	private void close() throws IOException{
-		if (streamIn != null){
-			streamIn.close();
-		}
-		if (socket != null){
-			socket.close();
-		}
-		if (service != null){
-			service.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		ChatServer server = null;
-	    if (args.length != 1)
-	         System.out.println("Usage: java ChatServer port");
-	    else
-	         server = new ChatServer(Integer.parseInt(args[0]));
+		if (args.length != 1)
+			System.out.println("Usage: java ChatServer port");
+		else
+			server = new ChatServer(Integer.parseInt(args[0]));
 	}
 }
